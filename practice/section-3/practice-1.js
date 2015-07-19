6162e224ef43fcab72a2e63c0ae3bde2ef7e0e9c
anlin-yang <anlin0414@outlook.com>
2015-07-19 17:53:20 +0800

    Finished section 3.

--- practice/section-3/practice-1.js
+++ practice/section-3/practice-1.js
@@ -1,3 +1,8 @@
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+  for(var i = 0; i < collection_a.length; i++){
+	  if(object_b["value"].indexOf(collection_a[i].key) != -1){
+		  collection_a[i]["count"]--;	
+	  }
+  }
+  return collection_a;
 }
--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -1,3 +1,8 @@
-function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+function create_updated_collection(collection_a, object_b) {	
+    for(var i = 0; i < collection_a.length; i++){
+	    if(object_b["value"].indexOf(collection_a[i].key) != -1){
+			collection_a[i]["count"] = collection_a[i]["count"] - Math.floor(collection_a[i]["count"] / 3);
+		}	
+    }
+	return collection_a;
 }
